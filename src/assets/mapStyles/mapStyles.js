72e6e3c5782b09c38fc73e1aff0f2c418bc6b560
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-07-29 15:04:51 -0500

    converted application to use VueMapbox plugin

---
+++ src/assets/mapStyles/mapStyles.js
@@ -0,0 +1,162 @@
+export default {
+    style: {
+        version: 8,
+        sources: {
+            basemap: {
+                type: "vector",
+                url: "http://localhost:8086/data/basemap.json"
+            },
+            HRU: {
+                type: "vector",
+                url: "http://localhost:8085/data/new2.json"
+            }
+        },
+        "sprite": "",
+        "glyphs": "https://orangemug.github.io/font-glyphs/glyphs/{fontstack}/{range}.pbf",
+        "layers": [
+            {
+                "id": "bg",
+                "type": "background",
+                "paint": {
+                    "background-color": "rgba(202, 210, 211, 1)"
+                }
+            },
+            {
+                "id": "states",
+                "type": "fill",
+                "source": "basemap",
+                "source-layer": "states",
+                "paint": {
+                    "fill-color": "rgba(246, 246, 244, 1)"
+                }
+            },
+            {
+                "id": "neighbors",
+                "type": "fill",
+                "source": "basemap",
+                "source-layer": "neighboringcountry",
+                "layout": {},
+                "paint": {
+                    "fill-color": "rgba(246, 246, 244, 1)"
+                }
+            },
+            {
+                id: "HRUS2",
+                type: "fill",
+                source: "HRU",
+                "source-layer": "no_simp_prec5",
+                paint: {
+                    "fill-color": {
+                        "property": "SoilMoisture",
+                        "type": 'categorical',
+                        "stops": [
+                            ["","#000000"],
+                            ["very low","#CC4C02"],
+                            ["low", "#EDAA5F"],
+                            ["average","#FED98E"],
+                            ["high","#A7B9D7"],
+                            ["very high","#144873"],
+                        ]
+                    },
+                    "fill-opacity": 1
+                }
+            },
+
+            {
+                id: "HRUS",
+                type: "line",
+                source: "HRU",
+                "source-layer": "no_simp_prec5",
+                paint: {
+                    "line-color": {
+                        "property": "SoilMoisture",
+                        "type": 'categorical',
+                        "stops": [
+                            ["","#000000"],
+                            ["very low","#823102"],
+                            ["low", "#C28C4E"],
+                            ["average","#D0B275"],
+                            ["high","#8998B0"],
+                            ["very high","#113B5F"],
+                        ]
+                    },
+                    "line-width": 1
+                },
+            },
+            {
+                "id": "rivers",
+                "type": "line",
+                "source": "basemap",
+                "source-layer": "USA_Rivers_and_Streams",
+                "minzoom": 5,
+                "layout": {},
+                "paint": {
+                    "line-color": "rgba(115, 255, 255, 1)"
+                }
+            },
+            {
+                "id": "counties",
+                "type": "line",
+                "source": "basemap",
+                "source-layer": "counties",
+                "minzoom": 6,
+                "maxzoom": 24,
+                "paint": {
+                    "line-color": "rgba(115, 255, 255, 1)"
+                }
+            },
+            {
+                "id": "statesBorder",
+                "type": "line",
+                "source": "basemap",
+                "source-layer": "states",
+                "layout": {},
+                "paint": {
+                    "line-color": "rgba(115, 255, 255, 1)",
+                    "line-dasharray": [
+                        2,
+                        1.5
+                    ]
+                }
+            },
+            {
+                "id": "citiesDot",
+                "type": "circle",
+                "source": "basemap",
+                "source-layer": "Cities_and_Towns_NTAD",
+                "minzoom": 6,
+                "paint": {
+                    "circle-radius": 4
+                }
+            },
+            {
+                "id": "cities",
+                "type": "symbol",
+                "source": "basemap",
+                "source-layer": "Cities_and_Towns_NTAD",
+                "minzoom": 6,
+                "layout": {
+                    "text-field": "{NAME}",
+                    "text-font": [
+                        "Roboto Regular"
+                    ],
+                    "text-size": 12,
+                    "symbol-placement": "point",
+                    "text-line-height": 1.2,
+                    "text-justify": "center",
+                    "text-anchor": "bottom",
+                    "text-offset": [
+                        0,
+                        -0.5
+                    ]
+                },
+                "paint": {
+                    "text-color": "rgba(255,255,255, 1)",
+                    "text-halo-width": 1,
+                    "text-halo-blur": 1,
+                    "text-halo-color": "rgba(0, 0, 0, 0.5)",
+                }
+            }
+        ]
+    }
+};
--- src/components/Visualization.vue
+++ src/components/Visualization.vue
@@ -1,22 +1,88 @@
 <template>
-    <div id="visualization">
-        <h1>{{ title }}</h1>
-        <img alt="USGS Logo" src="../assets/USGS_logo_green.svg">
+  <div id="viz_container">
+    <div class="usa-prose">
+      <h2 class="title-text">
+        {{ title }}
+      </h2>
     </div>
+    <hr>
+
+    <nav id="menu"></nav>
+    <MglMap id="map"
+        :accessToken="accessToken"
+        :container="container"
+        :mapStyle="mapStyle"
+        :zoom="zoom"
+        :minZoom="minZoom"
+        :maxZoom="maxZoom"
+        :center="center"
+        :attributionControl="false"
+    >
+      <MglScaleControl position="bottom-right" />
+      <MglNavigationControl position="bottom-right" />
+      <MglGeolocateControl position="bottom-right" />
+      <MglFullscreenControl position="bottom-right" />
+    </MglMap>
+
+  </div>
 </template>
 
 <script>
+    import {
+        MglMap,
+        MglNavigationControl,
+        MglGeolocateControl,
+        MglFullscreenControl,
+        MglScaleControl
+    } from "vue-mapbox";
+    import mapStyles from '../assets/mapStyles/mapStyles';
+
     export default {
-        name: 'Visualization',
+        components: {
+            MglMap,
+            MglNavigationControl,
+            MglGeolocateControl,
+            MglFullscreenControl,
+            MglScaleControl
+        },
+        name: 'MapBox',
         props: {
             title: {
                 type: String,
-                default: 'Add your title in App.vue or hide this with CSS'
+                default: 'Add your title in App.vue or make this blank'
+            }
+        },
+        data() {
+            return {
+                accessToken: 'pk.eyJ1IjoiYWJyaWdncyIsImEiOiJjandrd3J4bmMwcjNpNGFxZ2hoZGE2djR5In0.gKPNdhLzvbzui4aiIwAihA',
+                mapStyle: mapStyles.style,
+                container: 'map',
+                zoom: 4,
+                minZoom: 4,
+                maxZoom: 8,
+                center: [-95.7129, 37.0902]
             }
         }
     }
 </script>
 
+
 <style scoped lang="scss">
+  @import"~mapbox-gl/dist/mapbox-gl.css";
+
+  #map {
+    height: 900px;
+  }
+
+  .title-text {
+    margin-left: 1.5rem;
+    padding-top: 0.5rem;
+  }
+
+  hr {
+    margin: 2px 0 0 0;
+    padding-bottom: 0;
+  }
 
 </style>
+
